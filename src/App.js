4a63e6d68c6c5da76ce3be0e349e666eac9ff2e5
Abuk974 <120749240+Abuk974@users.noreply.github.com>
2023-06-22 09:29:41 +0300

    Added repo Url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>The Weather App</h1>
+      The Weather App
+      <footer>
+        This project was code by Abuk Sedonia and it's {""}
+        <a href="https://github.com/Abuk974/my-app-frank" target="blank"> 
+        open-sourced on Github             
+        </a>
+      </footer>
     </div>
   );
 }

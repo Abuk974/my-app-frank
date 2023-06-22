a561951959ddbb9dc5005613a54411b53b9ede39
Abuk974 <120749240+Abuk974@users.noreply.github.com>
2023-06-22 09:50:28 +0300

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      The Weather App
+      <div className="container">
+        
+      <h1>The Weather App</h1>
       <footer>
         This project was code by Abuk Sedonia and it's {""}
         <a href="https://github.com/Abuk974/my-app-frank" target="blank"> 
         open-sourced on Github             
         </a>
       </footer>
+
+      </div>
+
     </div>
   );
 }
